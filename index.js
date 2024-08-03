17acae9010b2aba694ad4dfe6b3146b37a464dea
Pablo de la Cruz Liza <158772945+ithembaSAC@users.noreply.github.com>
2024-08-02 23:17:11 -0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,17 @@
+ var password ="ajedrez";  
+
+ function passcheck() {
+
+
+  if (document.getElementById('pass1').value != password ) { 
+   alert('Clave incorrecta!');
+   return false; 
+  }
+
+//     if (document.getElementById('pass1').value != password ) {
+//         alert ('Correct Password. Click OK to enter web');
+//     }   
+
+}
+
+
